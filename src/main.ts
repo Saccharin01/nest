73f8b4df56024d7f384e12ec0af369120eeeee94
Saccharin01 <chousik01@gmail.com>
2024-07-22 04:33:10 +0900

    fail

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors(); // CORS 설정
+  await app.listen(3010);
+}
+bootstrap();
